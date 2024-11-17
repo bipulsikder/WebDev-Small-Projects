cc8d44d170efe66798a96b4e26b755851239249d
Bipul <147647143+bipulsikder@users.noreply.github.com>
2024-11-17 23:06:33 +0530

    Update script.js

--- Weather APp/script.js
+++ Weather APp/script.js
@@ -3,6 +3,8 @@ const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchbox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather img");
+
 
 async function fetchData(city) {
   try {
@@ -11,12 +13,31 @@ async function fetchData(city) {
       throw new Error("City not found");
     }
     const data = await response.json();
-    console.log(data);
+
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = data.main.temp + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+    if(data.weather[0].main== "Clouds"){
+      weatherIcon.src = "images/clouds.png"
+    }
+    else if (data.weather[0].main== "Clear"){
+      weatherIcon.src = "images/clear.png"
+    }
+    else if (data.weather[0].main== "Rain"){
+      weatherIcon.src = "images/rain.png"
+    }
+    else if (data.weather[0].main== "Drizzle"){
+      weatherIcon.src = "images/drizzle.png"
+    }
+    else if (data.weather[0].main== "Mist"){
+      weatherIcon.src = "images/mist.png"
+    }
+
+    document.querySelector(".weather").style.display = "block";
+
+
   } catch (error) {
     console.error(error);
     document.querySelector(".city").innerHTML = "City not found!";
